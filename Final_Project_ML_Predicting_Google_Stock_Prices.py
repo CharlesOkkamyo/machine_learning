9d0f0ca065b6edc9c13e5b2477b60582fc632706
CharlesOkkamyo <94747284+CharlesOkkamyo@users.noreply.github.com>
2023-04-26 12:35:29 +0630

    Update Final_Project_ML_Predicting_Google_Stock_Prices.py

--- Final_Project_ML_Predicting_Google_Stock_Prices.py
+++ Final_Project_ML_Predicting_Google_Stock_Prices.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
 st.title('Uber pickups in NYC')
+data = pd.read_csv('google_stock_data.csv', index_col='Date', parse_dates=True)
